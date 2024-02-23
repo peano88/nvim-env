c4fe1ae731b26bf165d0a2551f4f281836b80a1a
peano88 <ilpeano@gmail.com>
2024-02-23 17:00:56 +0100

    add which-key description for fugitive and gerrit commands

--- lbnvim/fugitive.lua
+++ lbnvim/fugitive.lua
@@ -1,5 +1,19 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-vim.keymap.set("n", "<leader>gmc", function ()
-    vim.cmd.Git [[ config --global user.email lorenzo.bompani@ingenico.com ]]
-    vim.cmd.Git [[ config --global user.name Lorenzo BOMPANI ]]
+vim.keymap.set("n", "<leader>gp", function ()
+    vim.cmd.Git [[ pull --rebase ]]
 end)
+vim.keymap.set("n", "<leader>gr", function ()
+    vim.cmd.split()
+    vim.api.nvim_command("term git review")
+end)
+
+local wk = require("which-key")
+
+wk.register({
+    g = {
+        name = "Git",
+        s = { "Status" },
+        p = { "Pull rebase" },
+        r = { "Review" },
+    }
+}, { prefix = "<leader>" })
