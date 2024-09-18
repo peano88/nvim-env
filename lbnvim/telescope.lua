8a4d1aa005eac0528b5c93f1b25eee1a2e9a2f57
peano88 <ilpeano@gmail.com>
2024-09-18 11:35:32 +0200

    fix  keymap of lsp document symbols

--- lbnvim/telescope.lua
+++ lbnvim/telescope.lua
@@ -35,7 +35,7 @@ telescope.load_extension "file_browser"
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fp', builtin.document_symbols, {})
+vim.keymap.set('n', '<leader>fp', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.live_grep({ glob_pattern = "!.git/*" });
 end)
